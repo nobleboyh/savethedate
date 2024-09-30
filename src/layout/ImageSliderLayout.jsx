14ba00147682fcfdf4791befdc0a01a1d96e86da
nobleboyh <hoangat3112@gmail.com>
2024-09-30 17:40:17 +0700

    Modify deploy info

--- src/layout/ImageSliderLayout.jsx
+++ src/layout/ImageSliderLayout.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Fade, Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
-
-
+const slideImages = import.meta.glob('/src/assets/images/slide/*.{png,jpg,jpeg,gif,svg}', { eager: true, as: 'url' });
 
 const ImageSliderLayout = () => {
-  const slideImages = import.meta.glob('/src/assets/images/slide/*.{png,jpg,jpeg,gif,svg}', { eager: true, as: 'url' });
   console.log(slideImages);
     return (
         <div className="slide-container">
